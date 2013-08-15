50893ba13c07ec4b134cf29537ac623b59affe0c
jbanford <jbanford@me.com>
2013-08-15 11:21:19 +0100

    Add logging tags

--- src/com/strath/hub/Hub.java
+++ src/com/strath/hub/Hub.java
@@ -9,10 +9,14 @@ import android.os.Bundle;
  */
 public class Hub extends Activity
 {
+  public static final String TAG = "Hub";
+	private static final boolean Debug = true;
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
+  	if(Debug) Log.i(TAG, "onCreate called.");
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
   }
