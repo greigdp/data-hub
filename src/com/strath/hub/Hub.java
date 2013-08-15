a12a641a2f1b9be16591e77791c6ecc675dc944b
jbanford <jbanford@me.com>
2013-08-15 13:30:48 +0100

    Stub methods for Bluetooth link set up

--- src/com/strath/hub/Hub.java
+++ src/com/strath/hub/Hub.java
@@ -1,7 +1,11 @@
 package com.strath.hub;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 /** 
  * The main activity and entry-point of the app.
@@ -12,12 +16,80 @@ public class Hub extends Activity
   public static final String TAG = "Hub";
 	private static final boolean Debug = true;
 
+	private BluetoothAdapter mBluetoothAdapter = null;
+	private BluetoothLinkService mLinkService = null;
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
   	if(Debug) Log.i(TAG, "onCreate called.");
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+
+    // Get the local Bluetooth adapter and check that Bluetooth is
+    // supported.
+    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter == null)
+    {
+    	Toast.makeText( this, 
+    		             "Bluetooth is not supported.", 
+    		             Toast.LENGTH_LONG ).show();
+    	finish();
+    	return;
+    }
+    else
+    {
+    	// If Bluetooth is not on, request that it be enabled.
+    	// setupLink() will then be called during onActivityResult.
+    	if (!mBluetoothAdapter.isEnabled())
+    	{
+    		Intent enableIntent = new Intent(BluetoothAdapter.
+    			                               ACTION_REQUEST_ENABLE);
+
+    	}
+    	// Bluetooth is on so set up the link seesion.
+    	else
+    	{
+        if (mLinkService == null)
+        	setupLink();
+        connectDevice();
+    	}
+    }
   }
+
+  @Override
+  public void onStart()
+  {
+  	super.onStart();
+  	if (Debug) Log.i(TAG, "onStart called.");
+  }
+
+  /**
+   * Initalise a {@codeBluetoothLinkService} object and call
+   * {@codeconnectDevice} to initiate a Bluetooth connection.
+   *
+   * @see #BluetoothLinkservice(Activity, Handler)
+   * @see #connectDevice()
+   */
+  private void setupLink()
+  {
+    if(Debug) Log.i(TAG, "setupLink called.");
+
+    // Stub method for now.
+  }
+
+  /**
+   * Create a {@codeBluetoothDevice} object representing the slave device,
+   * and connect to the device.
+   * 
+   * @see BluetoothDevice
+   * @see BluetoothLinkservice#connect(BluetoothDevice)
+   */
+  private void connectDevice()
+  {
+  	if(Debug) Log.i(TAG, "connectDevice called");
+  }
+
 }
