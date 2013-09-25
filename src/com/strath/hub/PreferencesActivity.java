a674c0fdd3458283d33547ca8bff3091e179c319
Greig Paul <greig.paul.2013@uni.strath.ac.uk>
2013-09-25 13:58:27 +0100

    Add support for a basic preferences area

--- src/com/strath/hub/PreferencesActivity.java
+++ src/com/strath/hub/PreferencesActivity.java
@@ -1,7 +1,28 @@
 package com.strath.hub;
 
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.os.Bundle;
+
 /**
  * Created by greig on 25/09/2013.
  */
-public class PreferencesActivity {
+public class PreferencesActivity extends PreferenceActivity
+{
+    @Override
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.prefs);
+        }
+    }
 }
